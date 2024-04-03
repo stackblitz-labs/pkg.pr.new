00ac857bd6eb8d0570417ac4ffeaf21098a66e32
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-04-04 00:03:42 +0330

    huge update

--- packages/backend/script/ci.ts
+++ packages/backend/script/ci.ts
@@ -3,6 +3,7 @@ if (process.env.CI) {
   const content = Object.entries(process.env)
     .filter(([k]) => k.startsWith("NITRO"))
     .map(([k, v]) => `${k}=${v}`)
+    .concat(['NITRO_TEST', "true"])
     .join("\n");
   await fs.writeFile(".dev.vars", content);
 }
