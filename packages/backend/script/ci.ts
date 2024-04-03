53453e668c425d7466136d312b4a0d3591829724
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-04-04 00:36:43 +0330

    debug

--- packages/backend/script/ci.ts
+++ packages/backend/script/ci.ts
@@ -2,7 +2,7 @@ import fs from "fs/promises";
 if (process.env.CI) {
   const content = Object.entries(process.env)
     .filter(([k]) => k.startsWith("NITRO"))
-    .map(([k, v]) => `${k}=${v}`)
+    .map(([k, v]) => `${k}="${v}"`)
     .concat(['NITRO_TEST=true', 'GITHUB_TOKEN=' + process.env.GITHUB_TOKEN!])
     .join("\n");
   console.log(content)
