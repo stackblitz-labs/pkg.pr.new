8054f904a90c812ce60100b56dc28a8fc4d6727f
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-04-04 00:28:41 +0330

    debug

--- packages/backend/script/ci.ts
+++ packages/backend/script/ci.ts
@@ -2,9 +2,9 @@ import fs from "fs/promises";
 if (process.env.CI) {
   const content = Object.entries(process.env)
     .filter(([k]) => k.startsWith("NITRO"))
-    .map(([k, v]) => `${k}=${v}`)
     .concat(['NITRO_TEST', "true"])
     .concat(['GITHUB_TOKEN', process.env.GITHUB_TOKEN!])
+    .map(([k, v]) => `${k}=${v}`)
     .join("\n");
   console.log(content)
   await fs.writeFile(".dev.vars", content);
