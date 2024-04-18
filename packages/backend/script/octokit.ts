246508406c5006c6e6f0b92dd9abdc0415f639dc
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-04-18 09:02:51 +0330

    init

---
+++ packages/backend/script/octokit.ts
@@ -0,0 +1,16 @@
+import esbuild from "esbuild";
+import { fileURLToPath } from "node:url";
+import { polyfillNode } from "esbuild-plugin-polyfill-node";
+
+await esbuild.build({
+  entryPoints: [fileURLToPath(import.meta.resolve("octokit/dist-web"))],
+  bundle: true,
+  format: "esm",
+  sourcemap: "inline",
+  platform: "browser",
+  write: true,
+  outfile: "vendor/octokit.build.mjs",
+	plugins: [
+  	polyfillNode(),
+	],
+});
