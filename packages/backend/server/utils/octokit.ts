19aa9e76f13bdd0b2e178e1b24fcd5a2a67a60fe
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-04-18 09:02:51 +0330

    init

--- packages/backend/server/utils/octokit.ts
+++ packages/backend/server/utils/octokit.ts
@@ -1,7 +1,6 @@
 import type { H3Event } from "h3";
 import type { App as AppType } from "octokit";
-// @ts-ignore
-import { App } from "../vendor/octokit.mjs";
+import { App } from "../../vendor/octokit.build.mjs";
 
 export function useOctokitApp(event: H3Event): AppType {
   const { appId, privateKey, webhookSecret } = useRuntimeConfig(event);
