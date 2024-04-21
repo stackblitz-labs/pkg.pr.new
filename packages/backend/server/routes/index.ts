d49e8f96911bb18cbec3fe2bfcb1dbbed8c3c339
AmirSa12 <amirhosseinpr184@gmail.com>
2024-04-21 20:01:11 +0330

    type

--- packages/backend/server/routes/index.ts
+++ packages/backend/server/routes/index.ts
@@ -1,4 +1,4 @@
-import { H3Event, sendRedirect } from "h3";
+import type { H3Event } from "h3";
 
 export default eventHandler((event: H3Event) => {
   return sendRedirect(event, "https://github.com/stackblitz-labs/pkg.pr.new");
