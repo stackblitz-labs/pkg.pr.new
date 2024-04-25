94c0618a2f6132e9b5a6eef6e64b8f7729845eb8
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-04-25 11:25:39 +0330

    debug

--- packages/backend/script/update-webhook-url.ts
+++ packages/backend/script/update-webhook-url.ts
@@ -1,8 +1,35 @@
 // import { Octokit } from "";
-import { Octokit } from "@octokit/action";
+import { createTokenAuth } from "@octokit/auth-token";
+import { Octokit, App } from "octokit";
+import { config } from "dotenv";
+import jwt from "jsonwebtoken";
 
-console.log('token', process.env.GITHUB_TOKEN)
-const octokit = new Octokit();
+config({ path: ".dev.vars" });
+
+function generateJWT() {
+  const payload = {
+    iat: Math.floor(Date.now() / 1000), // Issued at time
+    exp: Math.floor(Date.now() / 1000) + 10 * 60, // JWT expiration time (10 minutes from now)
+    iss: process.env.NITRO_APP_ID!, // Issuer (GitHub App ID)
+  };
+
+  return jwt.sign(payload, process.env.NITRO_PRIVATE_KEY!, {
+    algorithm: "RS256",
+  });
+}
+
+// Generate JWT
+const jwtToken = generateJWT();
+console.log(jwtToken);
+
+// console.log("token", createTokenAuth(process.env.GITHUB_TOKEN!)());
+// const { octokit } = new App({
+//   appId: process.env.NITRO_APP_ID,
+//   p
+// });
+const octokit = new Octokit({
+  auth: jwtToken,
+});
 
 let {
   data: { url },
@@ -10,10 +37,15 @@ let {
 
 console.log(`current webhook url: ${url}`);
 
-await octokit.request("PATCH /app/hook/config", {
-  url: "https://example.com/webhook",
-});
+const newUrl = new URL(
+  "/webhook",
+  process.env.API_URL ?? "https://stackblitz-cr.pages.dev/",
+);
 
-({ url } = await octokit.request("GET /app/hook/config"));
+({
+  data: { url },
+} = await octokit.request("PATCH /app/hook/config", {
+  url: newUrl.href,
+}));
 
 console.log(`webhook url is ${url} now!`);
