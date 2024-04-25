cf140100f9310c09292bdacf0c2b312475389bc6
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-04-25 08:56:38 +0330

    debug

--- packages/backend/script/update-webhook-url.ts
+++ packages/backend/script/update-webhook-url.ts
@@ -1,30 +1,21 @@
-import { App } from "octokit";
+import { Octokit } from "octokit";
 
-const { octokit, getInstallationOctokit } = new App({
-  appId: process.env.NITRO_APP_ID!,
-  privateKey: process.env.NITRO_PRIVATE_KEY!,
+const octokit = new Octokit({
+  // appId: process.env.NITRO_APP_ID!,
+  // privateKey: process.env.NITRO_PRIVATE_KEY!,
+  auth: process.env.GITHUB_TOKEN
 });
-
-const { data: installationData } = await octokit.request(
-  "GET /repos/{owner}/{repo}/installation",
-  {
-    owner: "stackblitz-labs",
-    repo: "pkg.pr.new",
-  },
-);
-
-const installation = await getInstallationOctokit(installationData.id);
 
 let {
   data: { url },
-} = await installation.request("GET /app/hook/config");
+} = await octokit.request("GET /app/hook/config");
 
 console.log(`current webhook url: ${url}`);
 
-await installation.request("PATCH /app/hook/config", {
+await octokit.request("PATCH /app/hook/config", {
   url: "https://example.com/webhook",
 });
 
-({ url } = await installation.request("GET /app/hook/config"));
+({ url } = await octokit.request("GET /app/hook/config"));
 
 console.log(`webhook url is ${url} now!`);
