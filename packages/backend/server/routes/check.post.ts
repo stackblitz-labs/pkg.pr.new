75aff1c3616fb02edfba5e90f86faeb8faed4600
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-05-20 23:48:07 +0330

    debug

--- packages/backend/server/routes/check.post.ts
+++ packages/backend/server/routes/check.post.ts
@@ -4,16 +4,12 @@ export default eventHandler(async (event) => {
 
   const app = useOctokitApp(event);
 
-  const { status } = await app.octokit.request(
-    "GET /repos/{owner}/{repo}/installation",
-    {
+  try {
+    await app.octokit.request("GET /repos/{owner}/{repo}/installation", {
       owner: owner,
       repo: repo,
-    },
-  );
-
-  console.log(owner, repo, status);
-  if (status !== 200) {
+    });
+  } catch {
     throw createError({
       statusCode: 404,
       message: `The app https://github.com/apps/pkg-pr-new is not installed on ${owner}/${repo}.`,
