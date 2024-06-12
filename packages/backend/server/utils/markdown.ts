c0aa1f3db1c17ac2596610d5a6afe03727b50c87
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-06-13 00:31:35 +0330

    shorter sha

--- packages/backend/server/utils/markdown.ts
+++ packages/backend/server/utils/markdown.ts
@@ -1,3 +1,4 @@
+import { abbreviateCommitHash } from "@pkg-pr-new/utils";
 import { WorkflowData } from "../types";
 
 export function generateCommitPublishMessage(
@@ -95,7 +96,7 @@ export function generatePublishUrl(
   workflowData: WorkflowData,
   compact: boolean,
 ) {
-  const tag = base === "sha" ? workflowData.sha : workflowData.ref;
+  const tag = base === "sha" ? abbreviateCommitHash(workflowData.sha) : workflowData.ref;
   const shorter = workflowData.repo === packageName;
 
   const urlPath = compact
