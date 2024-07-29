1e7838062447c0d44e47ca93485533baf304ae95
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-07-29 12:07:08 +0330

    init

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -331,7 +331,7 @@ const main = defineCommand({
   },
 });
 
-runMain(main);
+runMain(main).then(() => console.log('done'));
 
 // TODO: we'll add support for yarn if users hit issues with npm
 async function resolveTarball(pm: "npm" | "pnpm", p: string) {
