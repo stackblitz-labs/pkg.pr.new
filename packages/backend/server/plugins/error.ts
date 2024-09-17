a2c1a420e0085f0756f0175ccbc298fb3116da4c
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-09-17 18:26:17 +0330

    update

--- packages/backend/server/plugins/error.ts
+++ packages/backend/server/plugins/error.ts
@@ -1,5 +1,5 @@
 export default defineNitroPlugin((nitro) => {
   nitro.hooks.hook("error", async (error, { event }) => {
-    console.error(`${event?.path} Application error:`, error);
+    console.log(`${event?.path} Application error:`, error);
   });
 });
