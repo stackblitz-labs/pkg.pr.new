29a95f0503d1264fc6aa61e350ab108f748618e4
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-09-21 18:29:35 +0330

    update

--- packages/backend/server/plugins/error.ts
+++ packages/backend/server/plugins/error.ts
@@ -1,5 +1,27 @@
+const url = "https://events.baselime.io/v1/logs";
+const apiKey = "76740dd3171dedb44ce3a8429da9c9e902dac764"; // Your API key
+
 export default defineNitroPlugin((nitro) => {
   nitro.hooks.hook("error", async (error, { event }) => {
-    console.log(`${event?.path} Application error:`, error);
+    console.error(`${event?.path} Application error:`, error);
+
+    const payload = [
+      {
+        path: event?.path,
+        name: error.name,
+        message: error.message,
+        error: error.stack,
+      },
+    ];
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "x-api-key": apiKey,
+        "Content-Type": "application/json",
+        "x-service": "my-service",
+      },
+      body: JSON.stringify(payload),
+    });
   });
 });
