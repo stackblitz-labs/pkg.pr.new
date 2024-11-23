16de6cfe31a5057342b5ff83923cff59519986e7
AmirSa12 <amirhosseinpr184@gmail.com>
2024-11-23 22:06:24 +0330

    update

--- packages/backend/server/routes/stats.get.ts
+++ packages/backend/server/routes/stats.get.ts
@@ -7,18 +7,31 @@ export default eventHandler(async (event) => {
     let truncated = true;
     let cursor: string | undefined;
     let totalObjects = 0;
+    const orgs = new Set<string>();
+    const packages = new Set<string>();
 
     while (truncated) {
       const list = await binding.list({ cursor });
 
       totalObjects += list.objects.length;
+
+      for (const object of list.objects) {
+        const [org, pkg] = object.key.split(":");
+        orgs.add(org);
+        packages.add(pkg);
+      }
+
       truncated = list.truncated;
       cursor = list.cursor;
     }
 
+    console.log("All objects:", list.objects);
+
     return {
       ok: true,
       totalObjects,
+      totalOrgs: orgs.size,
+      totalPackages: packages.size,
     };
   } catch (error) {
     throw createError({
