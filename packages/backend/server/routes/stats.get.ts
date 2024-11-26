04dabb065b55288eba022dae6a810a97002962d6
AmirHossein Sakhravi <amirhosseinpr184@gmail.com>
2024-11-26 11:43:46 +0330

    fix: stream stats (#294)

--- packages/backend/server/routes/stats.get.ts
+++ packages/backend/server/routes/stats.get.ts
@@ -1,62 +1,87 @@
+import { sha256 } from "ohash"
+
 export default eventHandler(async (event) => {
   try {
     const binding = useBinding(event);
 
+    let started = false
+
     let cursor: string | undefined;
+
     let objectCount = 0;
-    let branches = 0
-    let prs = 0
-    const orgs = new Set<string>();
-    const repos = new Set<string>();
-    const commits = new Set<string>();
-    const packages = new Set<string>();
+
+    const encoder = new TextEncoder();
 
     const packagesPrefix = `${usePackagesBucket.base}:`;
     const cursorsPrefix = `${useCursorsBucket.base}:`;
 
-    do {
-      const response = await binding.list({ cursor });
-      objectCount += response.objects.length;
-
-      for (const { key } of response.objects) {
-        if (key.startsWith(packagesPrefix)) {
-          const trimmedKey = key.slice(packagesPrefix.length);
-          const [org, repo, commit, ...packageNameParts] = trimmedKey.split(":");
-          const packageName = packageNameParts.join(":");
-  
-          orgs.add(org);
-          repos.add(repo);
-          commits.add(commit);
-          packages.add(packageName);
-        } else if (key.startsWith(cursorsPrefix)) {
-          const trimmedKey = key.slice(cursorsPrefix.length);
-          const ref = trimmedKey.split(":")[2];
-          const prNumber = Number(ref)
-          if (!isNaN(prNumber)) {
-            prs++
-          } else {
-            branches++
-          }
+    const stream = new ReadableStream({
+      async pull(controller) {
+        if (started && !cursor) {
+          controller.enqueue(
+            encoder.encode(
+              JSON.stringify({
+                summary: {
+                  ok: true,
+                  objects: objectCount,
+                },
+              }) + "\n"
+            )
+          );
+          controller.close();
+          return;
         }
-      }
 
-      cursor = response.truncated ? response.cursor : undefined;
-    } while (cursor);
+        started = true
+        try {
+          const response = await binding.list({ cursor });
+          objectCount += response.objects.length;
 
-    return {
-      ok: true,
-      objects: objectCount,
-      orgs: orgs.size,
-      repos: repos.size,
-      commits: commits.size,
-      branches: branches,
-      prs: prs,
-      packages: packages.size,
-    };
+          for (const { key } of response.objects) {
+            let result = null;
+
+            if (key.startsWith(packagesPrefix)) {
+              const trimmedKey = key.slice(packagesPrefix.length);
+              const [org, repo, commit, ...packageNameParts] = trimmedKey.split(":");
+              const packageName = packageNameParts.join(":");
+
+              result = {
+                type: "package",
+                org: sha256(org),
+                repo: sha256(repo),
+                commit: sha256(commit),
+                packageName: sha256(packageName),
+              };
+            } else if (key.startsWith(cursorsPrefix)) {
+              const trimmedKey = key.slice(cursorsPrefix.length);
+              //TODO: merge parts like package names. 
+              const ref = trimmedKey.split(":")[2];
+
+              result = {
+                type: "cursor",
+                org: sha256(trimmedKey.split(":")[0]),
+                repo: sha256(trimmedKey.split(":")[1]),
+                ref: sha256(ref),
+              };
+            }
+
+            if (result) {
+              controller.enqueue(encoder.encode(JSON.stringify(result) + "\n"));
+            }
+          }
+
+          cursor = response.truncated ? response.cursor : undefined;
+        } catch (err) {
+          controller.error(err);
+        }
+      },
+    });
+
+    return sendStream(event, stream);
   } catch (error) {
     throw createError({
       statusCode: 500,
-      message: error instanceof Error ? error.message : String(error)
+      message: error instanceof Error ? error.message : String(error),
     });
   }
 });
