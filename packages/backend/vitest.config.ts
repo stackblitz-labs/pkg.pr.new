da916d09fb6683f77a1532ee35a4040248759cff
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2025-02-16 19:19:15 +0330

    update

--- packages/backend/vitest.config.ts
+++ packages/backend/vitest.config.ts
@@ -1,6 +1,8 @@
 import path from "path";
 import { defineConfig } from "vitest/config";
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 const simulacrumFoundationSimulator = path.resolve(
   __dirname,
   "node_modules/@simulacrum/foundation-simulator/dist/cjs/index.js",
