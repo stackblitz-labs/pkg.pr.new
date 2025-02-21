eeb5b097d796890cf439b8118ecd37ea3cbb83ed
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2025-02-21 07:46:38 +0330

    some logs to be sure!

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -547,7 +547,7 @@ runMain(main)
 type PackMethod = "npm" | "pnpm" | "yarn";
 
 async function resolveTarball(pm: PackMethod, p: string, pJson: PackageJson) {
-  console.log("pack", p, pJson);
+  console.log("pack", p, pm, pJson);
   let cmd = `${pm} pack`;
   let filename = `${pJson.name!.replace("/", "-")}-${pJson.version}.tgz`;
   if (pm === "yarn") {
@@ -557,6 +557,7 @@ async function resolveTarball(pm: PackMethod, p: string, pJson: PackageJson) {
     stdio: "overlapped",
     cwd: p,
   });
+  console.log("stdout", stdout);
   const lines = stdout.split("\n").filter(Boolean);
 
   if (pm !== "yarn") {
