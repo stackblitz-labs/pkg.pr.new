288a6800360964a114e1ee89e52aa6dc502cfa6a
AmirSa12 <amirhosseinpr184@gmail.com>
2025-05-12 11:03:46 +0330

    text

--- packages/app/app/pages/~/[owner]/[repo].vue
+++ packages/app/app/pages/~/[owner]/[repo].vue
@@ -79,7 +79,10 @@ useSeoMeta({
         />
       </div>
 
-      <div class="flex justify-center mt-2">
+      <div class="flex flex-col items-center justify-center mt-2">
+        <p class="text-sm mb-[10px]">
+          You can copy the badge and put in your README!
+        </p>
         <BadgeGenerator
           :owner="repository.owner.login"
           :repo="repository.name"
