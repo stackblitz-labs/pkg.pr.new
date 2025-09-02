6d66e1fb6aa04a33be953509b3bea9cfbf037585
AmirHossein Sakhravi <amirhosseinpr184@gmail.com>
2025-09-02 16:38:13 +0330

    fix: add pr state check for commenting (#399)

--- packages/app/server/routes/publish.post.ts
+++ packages/app/server/routes/publish.post.ts
@@ -1,6 +1,7 @@
 import type { H3Event } from "h3";
 import type { components as OctokitComponents } from "@octokit/openapi-types";
 import type { Comment, PackageManager } from "@pkg-pr-new/utils";
+import type { WorkflowData } from "../types";
 import { isPullRequest, isWhitelisted } from "@pkg-pr-new/utils";
 import { randomUUID } from "uncrypto";
 import { setItemStream, useTemplatesBucket } from "../utils/bucket";
@@ -231,7 +232,10 @@ export default eventHandler(async (event) => {
     checkRunUrl = html_url!;
   }
 
-  if (isPullRequest(workflowData.ref)) {
+  if (
+    isPullRequest(workflowData.ref) &&
+    (await getPullRequestState(installation, workflowData)) === "open"
+  ) {
     let prevComment: OctokitComponents["schemas"]["issue-comment"];
 
     await installation.paginate(
@@ -329,6 +333,25 @@ export default eventHandler(async (event) => {
     },
   };
 });
+
+async function getPullRequestState(
+  installation: Awaited<ReturnType<typeof useOctokitInstallation>>,
+  workflowData: WorkflowData,
+) {
+  try {
+    const { data: pr } = await installation.request(
+      "GET /repos/{owner}/{repo}/pulls/{pull_number}",
+      {
+        owner: workflowData.owner,
+        repo: workflowData.repo,
+        pull_number: Number(workflowData.ref),
+      },
+    );
+    return pr.state;
+  } catch (error) {
+    return null;
+  }
+}
 
 async function iterateAndDelete(
   event: H3Event,
